874a63dab8c466278228f9bb9ecf5d4bd7740fbb
Vedant <vedantnarwade121@gmail.com>
2025-11-20 20:09:50 +0530

    Fix: Add frontend files properly

---
+++ frontend/app/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { socket } from "./socket"
+
+export default function Home() {
+    const [isConnected, setIsConnected] = useState(false);
+    const [transport, setTransport] = useState("N/A");
+    
+    useEffect(() => {
+      if (socket.connected) {
+        onConnect()
+      }
+      function onConnect() {
+        setIsConnected(true);
+        setTransport(socket.io.engine.transport.name);
+
+        socket.io.engine.on("upgrade", (transport) => {
+          setTransport(transport.name);
+        });
+      }
+
+      function onDisconnect() {
+        setIsConnected(false);
+        setTransport("N/A");
+      }
+      socket.on("connect", onConnect);
+      socket.on("disconnect", onDisconnect);
+
+      return () => {
+        socket.off("connect", onConnect);
+        socket.off("disconnect", onDisconnect);
+      };
+    }, []);
+
+
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Status: {isConnected ? "Connected" : "Disconnected"}
+        </p>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Transport: {transport}
+        </p>
+      </div>
+    </div>
+    )
+}
